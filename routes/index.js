8c22c3c4099efa1c21ff22ee2890be77b79986ab
Five52 <five52@laposte.net>
2016-11-10 17:15:26 +0100

    Add mongo display beginnings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var assert = require('assert');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/mongo', (req, res, next) => {
+    let url = 'mongodb://localhost:27017/test';
+    let MongoClient = mongodb.MongoClient;
+
+    MongoClient.connect(url, (err, db) => {
+        assert.equal(null, err);
+        console.log('Connection established to', url);
+        let collection = db.collection('test');
+        db.collection('test').find().toArray((err, docs) => {
+            assert.equal(null, err);
+            res.render('mongo', {
+                message: 'Données en base',
+                docs: JSON.stringify(docs)
+            });
+        });
+    });
+})
 
 module.exports = router;
