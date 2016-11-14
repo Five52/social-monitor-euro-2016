c293058df5336b282c69c7e3bd8270f59be9df35
Five52 <five52@laposte.net>
2016-11-14 15:02:53 +0100

    Add sample reactions

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,32 @@ router.get('/insert', (req, res) => {
     });
 });
 
+router.get('/sample/reactions', (req, res) => {
+    res.json([
+        {
+            "_id": ObjectId("5827581bed3a7e1a3f32696f"),
+            "message": "Après Samuel Umtiti hier, Lucas Digne vient de signer un contrat de 5 ans avec le FC Barcelona ! 👊 #FiersdetreBleus"
+            "id": "112215632152510_1247865825254146",
+            "date": "2016-07-13T12:58:06+0000",
+            "angry": 35,
+            "haha": 234,
+            "like": 20088,
+            "love": 512,
+            "sad": 14,
+            "wow": 90
+        },{
+            "_id" : ObjectId("5827581bed3a7e1a3f326970"),
+            "message" : "On se sera bien marré quand même durant cet Euro ! Grâce à votre créativité, on aura pu voir quelques petites pépites comme ce remix pour Moussa Sissoko 🎤😂 #FiersdetreBleus",
+            "id" : "112215632152510_1247485368625525",
+            "date" : "2016-07-13T07:24:01+0000",
+            "angry" : 10,
+            "haha" : 1423,
+            "like" : 31682,
+            "love" : 1637,
+            "sad" : 7,
+            "wow" : 33
+        }
+    ]);
+});
+
 module.exports = router;
